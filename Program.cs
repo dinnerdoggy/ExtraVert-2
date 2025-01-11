7fee0abf760295a2957fd0bc944ec593beb29e96
Casey <dinnerdoggy@gmail.com>
2025-01-11 11:01:26 -0600

    modularized adding a plant

--- Program.cs
+++ Program.cs
@@ -94,23 +94,7 @@ void PerformOptionA()
 
 void PerformOptionB()
 {
-    Console.WriteLine("Please enter the species name:");
-    string species = Console.ReadLine();
-
-    Console.Write("Enter light needs 1-5:");
-    var LightNeeds = int.Parse(Console.ReadLine());
-
-    Console.WriteLine("Enter asking price:");
-    var AskingPrice = decimal.Parse(Console.ReadLine());
-
-    Console.WriteLine("Enter City: ");
-    var City = Console.ReadLine();
-
-    Console.WriteLine("Enter zip code: ");
-    var Zip = int.Parse(Console.ReadLine());
-
-    var newPlant = new Plant(species, LightNeeds, AskingPrice, City, Zip, false);
-    plants.Add(newPlant);
+    Options.PostPlant(plants);
 }
 
 static void PerformOptionC()
